af5ca931fdb591450c85f1af075963c9e32694e0
AlphaZoneR <frike98@yahoo.com>
2017-12-13 20:54:47 +0200

    added example for vec3 cross transform

---
+++ examples/transform_vec3_cross_product.py
@@ -0,0 +1,29 @@
+import ModernGL
+import numpy as np
+
+ctx = ModernGL.create_standalone_context()
+
+prog = ctx.program([
+    ctx.vertex_shader(
+    """
+        #version 330
+
+        in vec3 in_vec_1;
+        in vec3 in_vec_2;
+        out vec3 out_vec_cross; 
+        
+        void main(){
+            out_vec_cross = cross(in_vec_1, in_vec_2);
+        }
+
+    """
+    )
+    ], ['out_vec_cross'])
+
+vbo1 = ctx.buffer(np.array([1.0, 2.0, 3.0, 1.0, 1.0, 1.0]).astype('f4').tobytes())
+vbo2 = ctx.buffer(b'\x00' * int(vbo1.size / 2))
+
+vao = ctx.simple_vertex_array(prog, vbo1, ['in_vec_1', 'in_vec_2'])
+vao.transform(vbo2)
+
+print(np.frombuffer(vbo2.read(), dtype='f4'))
