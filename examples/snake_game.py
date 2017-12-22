182827c3072bf23cad44e3b04c157811a086acb3
AlphaZoneR <frike98@yahoo.com>
2017-12-22 11:05:18 +0200

    refracted

--- examples/snake_game.py
+++ examples/snake_game.py
@@ -1,8 +1,9 @@
+import random
+
 import ModernGL
-import time, random
-from math import cos, sin
-from miniglm import FloatArray, mat4_ortho, Vec2, mat4_perspective, mat4_look_at, mat4_ortho
+from miniglm import (FloatArray, Vec2)
 from ModernGL.ext.examples import run_example
+
 
 class Snake:
     def __init__(self):
@@ -12,21 +13,24 @@ class Snake:
         self.addLength()
         self.addLength()
         self.addLength()
+
     def initLength(self):
         self.length += 1
         self.body.append({
             "pos" : Vec2((25, 25))
         })
+
     def addLength(self):
         self.length += 1
         self.body.append({
             "pos" : Vec2((self.getX(), self.getY()))
         })
+
     def update(self, _x, _y):
         for i in range(self.length - 1, 0, -1):
             self.body[i]['pos'] = self.body[i - 1]['pos']
         self.body[0]['pos'] = Vec2((self.body[0]['pos'].tup[0] + _x, self.body[0]['pos'].tup[1] + _y))
-    
+
     def check(self):
         for b in self.body[1:]:
             if self.getX() == b['pos'].tup[0] and self.getY() == b['pos'].tup[1]:
@@ -34,106 +38,108 @@ class Snake:
                 return True
 
         return False
-    
+
     def reset(self):
         self.__init__()
-    
+
     def getX(self):
         return int(self.body[0]['pos'].tup[0])
-    
+
     def getY(self):
         return int(self.body[0]['pos'].tup[1])
 
+
 class Example:
+
     def __init__(self, wnd):
         self.wnd = wnd
         self.ctx = ModernGL.create_context()
         self.prog = self.ctx.program([
-            self.ctx.vertex_shader(
-                """
-                    #version 330
-                    uniform vec2 Push;
-                    uniform float ID;
-                    in vec2 in_verts;
-                    out float ID_OUT;
-                    void main(){
-                        float x = -1 + 2 * ((in_verts.x + Push.x) / 500);
-                        float y = -(-1 + 2 * ((in_verts.y + Push.y) / 500));
-                        ID_OUT = ID;
-                        gl_Position = vec4(x, y, 0, 1.0);
-                        
-                    }
-                """
-                ),
-            self.ctx.fragment_shader(
-                """
-                    #version 330
-                    in float ID_OUT;
-                    out vec4 color;
+            self.ctx.vertex_shader('''
+                #version 330
 
-                    void main(){
-                        if(ID_OUT == 1.0){
-                            color = vec4(0.2, 0.4, 0.7, 1.0);
-                        }else if(ID_OUT == 2.0){
-                            color = vec4(0.0, 0.0, 0.0, 1.0);
-                        }else if(ID_OUT == 3.0){
-                            color = vec4(1.0, 0.0, 0.0, 1.0);
-                        }
+                uniform vec2 Push;
+                uniform float ID;
+
+                in vec2 in_verts;
+
+                out float ID_OUT;
+
+                void main() {
+                    float x = -1 + 2 * ((in_verts.x + Push.x) / 500);
+                    float y = -(-1 + 2 * ((in_verts.y + Push.y) / 500));
+                    ID_OUT = ID;
+                    gl_Position = vec4(x, y, 0, 1.0);
+
+                }
+            '''),
+            self.ctx.fragment_shader('''
+                #version 330
+
+                in float ID_OUT;
+
+                out vec4 color;
+
+                void main(){
+                    if(ID_OUT == 1.0){
+                        color = vec4(0.2, 0.4, 0.7, 1.0);
+                    }else if(ID_OUT == 2.0){
+                        color = vec4(0.0, 0.0, 0.0, 1.0);
+                    }else if(ID_OUT == 3.0){
+                        color = vec4(1.0, 0.0, 0.0, 1.0);
                     }
-                """
-                )
+                }
+            ''')
         ])
 
         self.Push = self.prog.uniforms['Push']
         self.size = 50
         self.id = self.prog.uniforms['ID']
-        square = FloatArray(
-            (
-                0.0, 0.0,
-                0.0, 10,
-                10, 0.0,
 
-                0.0, 10,
-                10, 0.0,
-                10, 10,
-            )
-        )
+        square = FloatArray((
+            0.0, 0.0,
+            0.0, 10,
+            10, 0.0,
+
+            0.0, 10,
+            10, 0.0,
+            10, 10,
+        ))
 
         self.vbo = self.ctx.buffer(bytes(square))
         self.vao = self.ctx.simple_vertex_array(self.prog, self.vbo, ['in_verts'])
         self.map = [[0 for j in range(self.size)] for i in range(self.size)]
-        
+
         for i in range(self.size):
             self.map[i][0] = 1
             self.map[i][self.size - 1] = 1
             self.map[0][i] = 1
             self.map[self.size - 1][i] = 1
-        
+
         self.snake = Snake()
 
         self.pushval = (0, 0)
-        
+
         self.speedx = 0
         self.speedy = 0
-        
+
         self.foodx = int(random.random() * 100) % 49
         self.foody = int(random.random() * 100) % 49
-        
+
         self.gamespeed = 10
 
         while self.foodx == 0 or self.foody == 0:
             self.foodx = int(random.random() * 100) % 49
             self.foody = int(random.random() * 100) % 49
-        
-        
+
+
         self.lasttime = wnd.time
-    
+
     def render(self):
-        
         self.update()
         self.ctx.viewport = self.wnd.viewport
         self.ctx.clear(0.9, 0.9, 0.9)
-        
+
         self.id.value = 1.0
         for i in range(len(self.map)):
             for j in range(len(self.map[i])):
@@ -141,7 +147,7 @@ class Example:
                     self.pushval = (i * 10, j * 10)
                     self.Push.value = self.pushval
                     self.vao.render()
-        
+
         self.id.value = 2.0
         for b in self.snake.body:
             self.Push.value = (b['pos'].tup[0] * 10, b['pos'].tup[1] * 10)
@@ -159,13 +165,13 @@ class Example:
                 self.speedx = 0
                 self.speedy = 0
             self.lasttime = self.wnd.time
-        
+
         if self.map[self.snake.getX()][self.snake.getY()] == 1:
                 self.snake.reset()
                 self.gamespeed = 10
                 self.speedx = 0
                 self.speedy = 0
-        
+
         if self.snake.getX() == self.foodx and self.snake.getY() == self.foody:
             self.snake.addLength()
             self.gamespeed += 0.5
@@ -188,4 +194,4 @@ class Example:
             self.speedy = -1
             self.speedx = 0
 
-run_example(Example, size = (500, 500))
+run_example(Example, size=(500, 500))
--- examples/snake_game_3d.py
+++ examples/snake_game_3d.py
@@ -1,23 +1,27 @@
+import os
+import random
+
 import ModernGL
-import time, random, os
-from math import cos, sin, pi
-from miniglm import FloatArray, mat4_ortho, Vec2, mat4_perspective, mat4_look_at, mat4_ortho
+from miniglm import (Vec2, mat4_look_at,
+                     mat4_perspective)
 from ModernGL.ext.examples import run_example
 from ModernGL.ext.obj import Obj
+
 
 def local(*path):
     return os.path.join(os.path.dirname(__file__), *path)
 
 
 class Snake:
-    """
+    '''
         Snake class. Contains data regarding the length of the snake.
-    """
+    '''
     def __init__(self):
-        """
-            Constructor function which sets the length of the snake to 0, then creates the body, adding 3 parts to it,
-            so the starter snake has the length of 3.
-        """
+        '''
+            Constructor function which sets the length of the snake to 0, then
+            creates the body, adding 3 parts to it, so the starter snake has the
+            length of 3.
+        '''
         self.length = 0
         self.body = []
         self.initLength()
@@ -26,61 +30,65 @@ class Snake:
         self.addLength()
 
     def initLength(self):
-        """
-            Function, which adds the first bodypart to the body list. This function is required, because we place the head
-            of the snake in the middle of the map at coordinates (25, 25).
-        """
+        '''
+            Function, which adds the first bodypart to the body list. This
+            function is required, because we place the head of the snake in the
+            middle of the map at coordinates (25, 25).
+        '''
         self.length += 1
         self.body.append({
             "pos" : Vec2((25, 25))
         })
-    
+
     def addLength(self):
-        """
-            Function, which adds a bodypart to the body list and increases the length of the snake.
-        """
+        '''
+            Function, which adds a bodypart to the body list and increases the
+            length of the snake.
+        '''
         self.length += 1
         self.body.append({
             "pos" : Vec2((self.getX(), self.getY()))
         })
-    
+
     def update(self, _x, _y):
-        """
-            Function, which updates the position of each individual bodypart. Besides the head, each bodypart
-            will get the position of the bodypart before it. The heads position is updated by the speeds on the
-            oX and oY axes.
-        """
+        '''
+            Function, which updates the position of each individual bodypart.
+            Besides the head, each bodypart will get the position of the
+            bodypart before it. The heads position is updated by the speeds on
+            the oX and oY axes.
+        '''
         for i in range(self.length - 1, 0, -1):
             self.body[i]['pos'] = self.body[i - 1]['pos']
         self.body[0]['pos'] = Vec2((self.body[0]['pos'].tup[0] + _x, self.body[0]['pos'].tup[1] + _y))
-    
+
     def check(self):
-        """
-            Function, which checks wether the head of the snake is colliding with any other bodypart.
-        """
+        '''
+            Function, which checks wether the head of the snake is colliding
+            with any other bodypart.
+        '''
         for b in self.body[1:]:
             if self.getX() == b['pos'].tup[0] and self.getY() == b['pos'].tup[1]:
                 self.reset()
                 return True
 
         return False
-    
+
     def reset(self):
-        """
+        '''
             Function, which resets the state of the snake.
-        """
+        '''
         self.__init__()
-    
+
     def getX(self):
-        """
+        '''
             Function, which returns the X coordinate of the head.
-        """
+        '''
         return int(self.body[0]['pos'].tup[0])
-    
+
     def getY(self):
-        """
+        '''
             Function, which returns the Y coordinate of the head.
-        """
+        '''
         return int(self.body[0]['pos'].tup[1])
 
 class Example:
@@ -88,81 +96,80 @@ class Example:
         self.wnd = wnd
         self.ctx = ModernGL.create_context()
         self.prog = self.ctx.program([
-            self.ctx.vertex_shader(
-                """
-                    #version 330
-                    uniform mat4 MVP;
-                    uniform vec2 Push;
-                    uniform float ID;
-                    in vec3 in_verts;
-                    out float ID_OUT;
-                    void main(){
-                        ID_OUT = ID;
-                        gl_Position = MVP * vec4(in_verts.x + Push.x, in_verts.y + Push.y, in_verts.z, 1.0);
-                        
-                    }
-                """
-                ),
-            self.ctx.fragment_shader(
-                """
-                    #version 330
-                    in float ID_OUT;
-                    out vec4 color;
+            self.ctx.vertex_shader('''
+                #version 330
+                uniform mat4 MVP;
+                uniform vec2 Push;
+                uniform float ID;
 
-                    void main(){
-                        if(ID_OUT == 1.0){
-                            color = vec4(0.2, 0.4, 0.7, 1.0);
-                        }else if(ID_OUT == 2.0){
-                            color = vec4(0.4, .7 , 0.4, 1.0);
-                        }else if(ID_OUT == 3.0){
-                            color = vec4(1.0, 0.0, 0.0, 1.0);
-                        }else if(ID_OUT == 4.0){
-                            color = vec4(0.0, 0.0, 0.0, 1.0);
-                        }
+                in vec3 in_verts;
+
+                out float ID_OUT;
+
+                void main() {
+                    ID_OUT = ID;
+                    gl_Position = MVP * vec4(in_verts.x + Push.x, in_verts.y + Push.y, in_verts.z, 1.0);
+                }
+            '''),
+            self.ctx.fragment_shader('''
+                #version 330
+
+                in float ID_OUT;
+
+                out vec4 color;
+
+                void main() {
+                    if(ID_OUT == 1.0) {
+                        color = vec4(0.2, 0.4, 0.7, 1.0);
+                    }else if(ID_OUT == 2.0) {
+                        color = vec4(0.4, .7 , 0.4, 1.0);
+                    }else if(ID_OUT == 3.0) {
+                        color = vec4(1.0, 0.0, 0.0, 1.0);
+                    }else if(ID_OUT == 4.0) {
+                        color = vec4(0.0, 0.0, 0.0, 1.0);
                     }
-                """
-                )
+                }
+            ''')
         ])
 
         self.Push = self.prog.uniforms['Push']
         self.Mvp = self.prog.uniforms['MVP']
         self.size = 50
         self.id = self.prog.uniforms['ID']
-        
+
         sphere = Obj.open(local('data', 'sphere.obj'))
 
         self.vbo = self.ctx.buffer(sphere.pack('vx vy vz'))
         self.vao = self.ctx.simple_vertex_array(self.prog, self.vbo, ['in_verts'])
         self.map = [[0 for j in range(self.size)] for i in range(self.size)]
-        
+
         for i in range(self.size):
             self.map[i][0] = 1
             self.map[i][self.size - 1] = 1
             self.map[0][i] = 1
             self.map[self.size - 1][i] = 1
-        
+
         self.snake = Snake()
 
         self.pushval = (0, 0)
-        
+
         self.speedx = 0
         self.speedy = 0
-        
+
         self.foodx = int(random.random() * 100) % 49
         self.foody = int(random.random() * 100) % 49
-        
+
         self.gamespeed = 10
         self.paused = False
-        
+
         while self.foodx == 0 or self.foody == 0:
             self.foodx = int(random.random() * 100) % 49
             self.foody = int(random.random() * 100) % 49
-        
-        
+
+
         self.lasttime = wnd.time
-    
+
     def render(self):
-        
         self.update()
         self.ctx.viewport = self.wnd.viewport
         self.ctx.clear(0.9, 0.9, 0.9)
@@ -206,13 +213,13 @@ class Example:
                 self.speedx = 0
                 self.speedy = 0
             self.lasttime = self.wnd.time
-        
+
         if self.map[self.snake.getX()][self.snake.getY()] == 1:
-                self.snake.reset()
-                self.gamespeed = 10
-                self.speedx = 0
-                self.speedy = 0
-        
+            self.snake.reset()
+            self.gamespeed = 10
+            self.speedx = 0
+            self.speedy = 0
+
         if self.snake.getX() == self.foodx and self.snake.getY() == self.foody:
             self.snake.addLength()
             self.gamespeed += 0.5
@@ -241,4 +248,4 @@ class Example:
             else:
                 self.paused = True
 
-run_example(Example, size = (500, 500))
+run_example(Example, size=(500, 500))
