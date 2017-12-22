9be0ef13f5dae8c6e564ab5784771f1971720eeb
AlphaZoneR <frike98@yahoo.com>
2017-12-22 10:27:02 +0200

    refactor

--- examples/transform_float_example.py
+++ examples/transform_float_example.py
@@ -4,22 +4,20 @@ import numpy as np
 ctx = ModernGL.create_standalone_context()
 
 prog = ctx.program([
-    ctx.vertex_shader(
-    """
+    ctx.vertex_shader('''
         #version 330
 
         in float in_float;
         out float out_float; 
         
-        void main(){
+        void main() {
             out_float = in_float * 3.14;
         }
 
-    """
-    )
-    ], ['out_float'])
+    ''')
+], ['out_float'])
 
-vbo1 = ctx.buffer(np.array([23.15],).astype('f4').tobytes())
+vbo1 = ctx.buffer(np.array([23.15]).astype('f4').tobytes())
 vbo2 = ctx.buffer(b'\x00' * vbo1.size)
 
 vao = ctx.simple_vertex_array(prog, vbo1, ['in_float'])
